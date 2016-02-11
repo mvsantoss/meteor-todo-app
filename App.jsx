920b52c1f0f99bed1933fefaf9ddb9c2e1d11ef4
Marcus Santos <mvsantoss@gmail.com>
2016-02-11 12:17:48 -0200

    first commit

---
+++ App.jsx
@@ -0,0 +1,69 @@
+// App component - represents the whole app
+App = React.createClass({
+    // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
+
+  getInitialState() {
+    return {
+      hideCompleted: false
+    }
+  },
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: ! this.state.hideCompleted
+    });
+  },
+
+  getMeteorData() {
+    let query = {};
+    if (this.state.hideCompleted) {
+      // If hide completed is checked, filter tasks
+      query = {checked: {$ne: true}};
+    }
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+      incompleteCount: Tasks.find({checked: {$ne: true}}).count()
+    }
+  },
+
+  renderTasks() {
+    return this.data.tasks.map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // Find the text field via the React ref
+    var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List ({this.data.incompleteCount})</h1>
+          <label className="hide-completed">
+            <input type="checkbox" readOnly={true} checked={this.state.hideCompleted} onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
+
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+          </form>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+});
